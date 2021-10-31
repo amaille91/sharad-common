21a3786dfc740446ce59da476e038da45d7f2645
Alexandre Maille <blank_email>
2021-10-31 09:15:30 +0100

    model for checklists

--- Model.hs
+++ Model.hs
@@ -1,104 +1,75 @@
-{-# LANGUAGE DeriveGeneric         #-}
--- {-# LANGUAGE MultiParamTypeClasses #-}
--- {-# LANGUAGE AllowAmbiguousTypes   #-}
--- {-# LANGUAGE PolyKinds             #-}
--- {-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
-module Model (Note(..), NoteContent(..), NoteUpdate(..), StorageId(..), Checklist(..), ChecklistContent(..), ChecklistUpdate(..), ChecklistItem(..), Content, Update, Identifiable, storageId, GettableContent, getContent, ContentType) where
+module Model (
+  NoteContent(..), StorageId(..)
+  , ChecklistContent(..), ChecklistItem(..)
+  , Content
+  , Identifiable(..), hash
+  ) where
 
 
 import           Data.Aeson
-import           GHC.Generics (Generic)
-
-class Identifiable a where
-  storageId :: a -> StorageId
-
-class (Eq a, Show a, ToJSON a) => Content a where
- 
-class (Show a, ToJSON a, Content (UpdateContentType a)) => Update a where
-  type UpdateContentType a :: * 
-  targetId :: a -> StorageId
-  newContent :: a -> UpdateContentType a
-
-class (FromJSON a, Content (ContentType a)) => GettableContent a where
-  type ContentType a :: *
-  getContent ::  a -> ContentType a
-
-data NoteContent = NoteContent { title   :: Maybe String
-                               , content :: String
-                               } deriving(Show, Generic, Eq)
-
-instance Content NoteContent where
-instance Content ChecklistContent where
-
-instance FromJSON NoteContent
-instance ToJSON NoteContent
+import qualified Data.ByteString.Base64.Lazy as Base64 (encode)
+import           Data.ByteString.Lazy.Char8  as BL
+import           Data.Digest.Pure.SHA        (bytestringDigest, sha256)
+import           GHC.Generics                (Generic)
 
 data StorageId = StorageId { id      :: String
                            , version :: String
                            } deriving (Show, Generic, Eq)
 
-instance ToJSON StorageId
+instance ToJSON   StorageId
 instance FromJSON StorageId
 
-data Note = Note { noteId :: StorageId
-                 , noteContent :: NoteContent
-                 } deriving (Show, Generic, Eq)
+class (Show a, Eq a, ToJSON a, FromJSON a) => Content a where
+  hash :: a -> String
 
-instance ToJSON Note
-instance FromJSON Note
-instance Identifiable Note where
-  storageId = noteId
-instance GettableContent Note where
-  type ContentType Note = NoteContent
-  getContent = noteContent
+data Content a => Identifiable a =
+    Identifiable { storageId :: StorageId, content :: a } deriving (Eq, Show)
 
+instance Content a => ToJSON (Identifiable a) where
+  toJSON (Identifiable {..}) =
+    object [ "storageId" .= storageId, "content" .= content ]
 
-data NoteUpdate = NoteUpdate StorageId NoteContent deriving (Show, Generic)
+instance Content a => FromJSON (Identifiable a) where
+  parseJSON = withObject "Identifiable" $ \value -> Identifiable
+    <$> value .: "storageId"
+    <*> value .: "content"
 
-instance FromJSON NoteUpdate
-instance ToJSON NoteUpdate
-instance Update NoteUpdate where
-  type UpdateContentType NoteUpdate = NoteContent
-  targetId (NoteUpdate _id _) = _id
-  newContent (NoteUpdate _ content) = content
+-- ===================== Note =============================================
+
+data NoteContent = NoteContent { title       :: Maybe String
+                               , noteContent :: String
+                               } deriving (Show, Generic, Eq)
+
+instance Content NoteContent where
+  hash content = base64Sha256 $ show content
+
+instance FromJSON NoteContent
+instance ToJSON   NoteContent
 
 -- ======================= CHECKLIST =======================================
 
-data ChecklistContent = ChecklistContent { name :: String
+data ChecklistContent = ChecklistContent { name  :: String
                                          , items :: [ChecklistItem]
                                          } deriving (Show, Generic, Eq)
 
-instance FromJSON ChecklistContent
-instance ToJSON ChecklistContent
+data ChecklistItem    = ChecklistItem    { label   :: String
+                                         , checked :: Bool
+                                         } deriving (Show, Generic, Eq)
 
-data ChecklistItem = ChecklistItem { label :: String
-                                   , checked :: Bool
-                                   } deriving (Show, Generic, Eq)
+instance FromJSON ChecklistContent
+instance ToJSON   ChecklistContent
 
 instance FromJSON ChecklistItem
-instance ToJSON ChecklistItem
+instance ToJSON   ChecklistItem
 
-data Checklist = Checklist { checklistId :: StorageId
-                           , checklistContent :: ChecklistContent
-                           } deriving (Show, Generic, Eq)
+instance Content ChecklistContent where
+  hash checklistContent = base64Sha256 $ show checklistContent
 
-instance ToJSON Checklist
-instance FromJSON Checklist
-instance Identifiable Checklist where
-  storageId = checklistId
-instance GettableContent Checklist where
-  type ContentType Checklist = ChecklistContent
-  getContent = checklistContent
+-- =============================== Utils ==========================================
 
-
-data ChecklistUpdate = ChecklistUpdate StorageId ChecklistContent deriving (Show, Generic, Eq)
-
-instance FromJSON ChecklistUpdate
-instance ToJSON ChecklistUpdate
-instance Update ChecklistUpdate where
-  type UpdateContentType ChecklistUpdate = ChecklistContent
-  targetId (ChecklistUpdate _id _) = _id
-  newContent (ChecklistUpdate _ content) = content
+base64Sha256 :: String -> String
+base64Sha256 contentToHash = BL.unpack . Base64.encode . bytestringDigest . sha256 $ BL.pack contentToHash
